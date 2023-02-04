c2a73d80fe064509f5c797c24c1cfdabe423f71a
Tejas Jayashankar <tejaskj@gmail.com>
2023-02-04 00:28:43 -0500

    Added baseline bmshj

---
+++ maximal_correlation/configs/bmshj.py
@@ -0,0 +1,39 @@
+def get_config():
+    config = dict(
+        rd_lambda = 0.18,
+        model_config = dict(
+            augmentation_config = [
+                "NoAugmentation",
+            ],
+            encoder_config = [
+                "BMSHJEncoder",
+            ],
+            decoder_config = [
+                "BMSHJDecoder",
+            ],
+        ),
+        optimizer_e_config = [
+            "Adam",
+            dict(
+                lr=1e-4,
+            ),
+        ],
+        optimizer_ed_config = [
+            "Adam",
+            dict(
+                lr=1e-4,
+            ),
+        ],
+        optimizer_aux_config = [
+            "Adam",
+            dict(
+                lr=1e-3,
+            ),
+        ],
+        trainer_config = dict(
+            model_dir="checkpoints/bmshj",
+            distributed=False,
+            hscore_start=2_000_000,
+        ),
+    )
+    return config
